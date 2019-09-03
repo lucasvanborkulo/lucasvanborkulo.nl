bf4593bfca94b5b821554c32928b276982357b7f
lucasvanborkulo <lucasvanborkulo@icloud.com>
2019-09-03 14:53:55 +0200

    Initial commit

---
+++ resources/js/effects.js
@@ -0,0 +1,15 @@
+
+var title = "Lucas van Borkulo";
+var element = document.getElementById("title");
+
+for (var i=0; i<title.length; i++) {
+    (function(i, title) {
+        setTimeout(function () {
+            var nextChar = title.charAt(i);
+
+            element.textContent += nextChar;
+        }, i*400);
+    })(i, title);
+}
+
+
